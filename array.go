30a445e9f050c0a092aa37735af949771c30963b
wangjunjie <wangjunjie@banggood.com>
2020-04-15 11:08:26 +0800

    array

--- array.go
+++ array.go
@@ -1,19 +1,60 @@
-package main
+package  main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+// func test(x [2]int){
+// 	fmt.Printf("x: %p\n", &x)
+// 	x[1] = 1000
+// }
 
-func main(){
+// func main(){
+// 	a := [2]int{}
+// 	fmt.Printf("a: %p\n", &a)
+// 	test(a)
+// 	fmt.Println(a)
+// }
 
-	var f[2][3]int = [...][3]int{{1,2,3},{7,8,9}}
 
-	for k1,v1 := range f {
-		for k2,v2 := range v1 {
-			fmt.Printf("(%d,%d)=%d ",k1,k2,v2)
-		}
-		fmt.Println()
+
+// func printArr(arr *[5]int){
+// 	arr[0] = 10 
+// 	for i,v := range arr {
+// 		fmt.Println(i,v)
+// 	}
+// }
+
+
+// func main(){
+// 	var arr1 [5]int
+// 	printArr(&arr1)
+// 	fmt.Println(arr1)
+// 	arr2 := [...]int{2,4,6,8,10}
+// 	printArr(&arr2)
+// 	fmt.Println(arr2)
+// }
+
+
+
+func sumArr(a [10]int) int{
+	var sum int = 0 
+	for i :=0; i< len(a); i++ {
+		sum +=a[i]
 	}
+	return sum 
+}
 
+
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	var b [10]int
+	for i :=0; i < len(b); i++ {
+		b[i] = rand.Intn(1000)
+	}
+	sum := sumArr(b)
+	fmt.Printf("sum=%d\n",sum)
 }
